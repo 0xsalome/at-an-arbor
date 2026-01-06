3210d6c3aa94786942c137bb5ccc85d9e1a9a720
0xsalome <ouroboros836@gmail.com>
2026-01-06 10:26:50 +0900

    vault backup: 2026-01-06 10:26:49

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ViteRestart from 'vite-plugin-restart';
 
 export default defineConfig({
   base: '/at-an-arbor/',
@@ -8,7 +9,12 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteRestart({
+      restart: ['content/**/*.md'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
