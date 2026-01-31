cbe436d0177e13e915f5dcad0e6a61996d0fde92
R <112565123+0xsalome@users.noreply.github.com>
2026-01-31 21:46:39 +0000

    chore: announce essay update

--- scripts/announce.js
+++ scripts/announce.js
@@ -1,5 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get current directory in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Get message from command line arguments
 const message = process.argv[2];
